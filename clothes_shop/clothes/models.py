7b6c3c023cb9f8dc1b9bda2183dad4bc7d156086
Roman Loboda <lobodaroman777@gmail.com>
2024-12-26 17:32:43 +0100

    changed cloth models

--- clothes_shop/clothes/models.py
+++ clothes_shop/clothes/models.py
@@ -4,4 +4,6 @@ class Cloth(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=50)
     description = models.TextField()
-    image = models.ImageField(default = 'default.png')
+    price = models.FloatField(null=True)
+    image = models.ImageField()
+    category = models.TextField(null=True)
